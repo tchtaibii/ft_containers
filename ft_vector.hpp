1277a91fb87a5af03163de50ec57923512b671b6
Taha Chtaibi <tchtaibi@e2r5p1.1337.ma>
2023-01-08 16:54:43 +0100

    -_-

--- ft_vector.hpp
+++ ft_vector.hpp
@@ -1,33 +1,60 @@
 #ifndef FT_VECTOR_HPP
 #define FT_VECTOR_HPP
 
+#include <iostream>
 
 namespace ft{
-    template <typename T, typename Alloc = std::allocator<T>>
+    template <typename T, typename Alloc = std::allocator<T> >
     class vector{
         typedef T value_type;
         typedef Alloc allocator_type;
-        typedef allocator_type::reference reference;
-        typedef allocator_type::pointer pointer;
+        typedef T& reference;
+        typedef T* pointer;
+        typedef size_t size_type;
         
         public:
             // (constructor)
-            vector() : size_(0), capacity_(0), elements_(nullptr) {}
-            explicit vector (const allocator_type& alloc = allocator_type())
-    {};
-            explicit vector (size_type n, const T& val = T(), const allocator_type& alloc = allocator_type());	
+            explicit vector(const allocator_type& alloc = allocator_type()) : alloc(alloc), elements_(nullptr), size_(0), capacity_(0){
+            }
+            explicit vector(size_type n, const T& val = T(), const allocator_type& alloc = allocator_type())
+            {
+                this->alloc = alloc;
+                this->elements_ = alloc.allocate(n);
+                this->size_ = 0;
+                this->capacity_ = n;
+                while (n > 0)
+                {
+                    alloc.construct(&elements_[size_], val);
+                    size_++;
+                    n--;
+                }
+            }
             template <class InputIterator>
             vector (InputIterator first, InputIterator last,const allocator_type& alloc = allocator_type());	
             vector (const vector& x);
+            vector& operator=(const vector& x)
+            {
+                int n = x.size_;
+                this->size_ = 0;
+                this->capacity_ = x.capacity_;
+                this->elements_ = alloc.allocate(this->capacity_);
+                while (n > 0)
+                {
+                    alloc.construct(&elements_[size_], x.elements_[size_]);
+                    size_++;
+                    n--;
+                }
+                std::cout << "vector : " << elements_[size_] << std::endl;
+                return *this;
+            }
             // (destructor)
-            ~Vector() {}
-        public:
-        alloc ccc;
+            ~vector() {}
 
         private:
-            size_t size_;
-            size_t capacity_;
-            pointer* elements_;
+            allocator_type alloc;
+            size_type size_;
+            size_type capacity_;
+            pointer elements_;
     };
 };
 
---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include "ft_vector.hpp"
+
+int main()
+{
+    ft::vector<int> test(5, 2);
+    // ft::vector<int> test2;
+
+    // test2 = test;
+}
